d6518b8f01da03c567fbc9b272802b7bb667384b
maisahjuevesano <maisahu@live.se>
2022-11-04 14:53:21 +0100

    rensar lite kommentarer

--- src/js/script.js
+++ src/js/script.js
@@ -10,12 +10,10 @@ let addToDoButton = document.getElementById("addToDo");
 let divTag = document.getElementById("divId");
 let inputField = document.getElementById("inputField");
 
-showList();
+displayList();
 
-function showList() {
+function displayList() {
   divTag.innerHTML = "";
-  // inputField.value = "";
-  // inputField.innerHTML = "";
 
   for (let i = 0; i < toDoList.length; i++) {
     //inuti här ska jag hämta mina taggar med hjälp av mina id från html. "fångar dom med en variabel"
@@ -23,31 +21,25 @@ function showList() {
     ulTag.classList.add("ulTag");
 
     divTag.appendChild(ulTag);
-    // i min ul tag vill jag skapa li taggar
     let liTag = document.createElement("li");
-    // i min liTag vill jag sätta in min lista
     liTag.innerHTML = toDoList[i].task;
 
-    // i min ulTag vill jag stoppa in min liTag
-    ulTag.appendChild(liTag); // jag måste köra utan citat-tecken för att listorna ska skrivas ut i webben.
+    ulTag.appendChild(liTag);
 
-    //nu vill jag skapa en checkbox, det gör jag med hjälp av en variabel
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox"; //skapar attribute
     checkbox.classList.add("checkbox"); //skapar en class så jag kan styla den i sassfilen
-
-    //jag vill lägga in min checkbox i min liTag
     liTag.appendChild(checkbox);
 
     //skapar en deletebutton
     let deletebutton = document.createElement("button");
     deletebutton.innerHTML = '<i class="fa-regular fa-trash-can"></i>';
     deletebutton.classList.add("deletebutton");
-    liTag.appendChild(deletebutton); //lägger in min button i min litag så den dyker upp när en li skapas
+    liTag.appendChild(deletebutton);
 
     /***********************Här ska mina funktioner vara******************************/
 
-    //nu vil jag göra min checkbox en klickhändelse med hjälp av addEventListener
+    //vad som händer när jag klickar i min checkbox
     checkbox.addEventListener("click", () => {
       //Här inne skriver jag vad som ska hända när jag klickar
       if (checkbox.checked === true) {
@@ -63,12 +55,13 @@ function showList() {
     //min deleteknapp
     deletebutton.addEventListener("click", () => {
       toDoList.splice([i], 1);
-      showList();
+      displayList();
     });
   }
   console.log(toDoList);
 }
 
+//min lägg till knapp
 addToDoButton.addEventListener("click", () => {
   if (inputField.value === "") {
     alert("You need to write a task!");
@@ -77,9 +70,10 @@ addToDoButton.addEventListener("click", () => {
   }
 });
 
+//funktionen till min lägg till knapp
 function add() {
   let newToDo = new Tasks(inputField.value, false);
   toDoList.push(newToDo);
   inputField.value = "";
-  showList();
+  displayList();
 }
