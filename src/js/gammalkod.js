5234b066ff92b4a9802c24145a8084544cf4e6c0
maisahjuevesano <maisahu@live.se>
2023-03-23 18:06:09 +0100

    remove js file that's not being used

--- src/js/gammalkod.js
+++
@@ -1,9 +0,0 @@
-// addToDoButton.addEventListener("click", add);
-
-// //kommentera tillbaka ska testa lite
-
-// function add() {
-//   let newToDo = new Tasks(inputField.value, false);
-//   toDoList.push(newToDo);
-//   showList();
-// }
